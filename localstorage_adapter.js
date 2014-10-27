fc7c4094f21c9d3cb9a1d32bd571da1d69fe504b
Carter Parks <carter@carterparks.com>
2014-10-26 19:00:33 -0600

    changed back to fix determineRelationshipType bug

--- localstorage_adapter.js
+++ localstorage_adapter.js
@@ -8,7 +8,7 @@
     serializeHasMany: function(record, json, relationship) {
       var key = relationship.key;
       var payloadKey = this.keyForRelationship ? this.keyForRelationship(key, "hasMany") : key;
-      var relationshipType = record.constructor.determineRelationshipType(relationship);
+      var relationshipType = DS.RelationshipChange.determineRelationshipType(record.constructor, relationship);
 
       if (relationshipType === 'manyToNone' ||
           relationshipType === 'manyToMany' ||
